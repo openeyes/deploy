0b6b594ef39d5bf7d3b80e5cab410ea3c2e8fe67
Mark Wadham <m@rkw.me>
2014-11-27 13:57:47 +0000

    [OE-5154] removed model references and removed "Angle Closure" as Tim confirmed this is the same thing as "Angle closure glaucoma"

--- migrations/m141127_105011_g21_diagnoses_and_findings.php
+++ migrations/m141127_105011_g21_diagnoses_and_findings.php
@@ -4,20 +4,24 @@ class m141127_105011_g21_diagnoses_and_findings extends CDbMigration
 {
 	public function up()
 	{
-		$glaucoma = Subspecialty::model()->find('name=?',array('Glaucoma'));
+		$glaucoma_id = $this->dbConnection->createCommand()->select("id")->from("subspecialty")->where("name=:name",array(":name" => "Glaucoma"))->queryScalar();
 
-		foreach (Yii::app()->db->createCommand()->select("*")->from("common_ophthalmic_disorder")->where("subspecialty_id=$glaucoma->id")->queryAll() as $cod) {
-			Yii::app()->db->createCommand("delete from secondaryto_common_oph_disorder where parent_id = {$cod['id']}")->query();
+		foreach ($this->dbConnection->createCommand()->select("*")->from("common_ophthalmic_disorder")->where("subspecialty_id=$glaucoma_id")->queryAll() as $cod) {
+			$this->dbConnection->createCommand("delete from secondaryto_common_oph_disorder where parent_id = {$cod['id']}")->query();
 		}
 
-		Yii::app()->db->createCommand("delete from common_ophthalmic_disorder where subspecialty_id=$glaucoma->id")->query();
+		$this->dbConnection->createCommand("delete from common_ophthalmic_disorder where subspecialty_id=$glaucoma_id")->query();
 
-		$this->insert('disorder',array('id' => 104,'fully_specified_name' => 'Primary Ocular hypertension (disorder)','term' => 'Primary Ocular hypertension'));
-		$this->insert('disorder',array('id' => 105,'fully_specified_name' => 'Normal Tension Glaucoma (disorder)', 'term' => 'Normal Tension Glaucoma'));
-		$this->insert('disorder',array('id' => 106,'fully_specified_name' => 'Angle Closure (disorder)','term' => 'Angle Closure'));
-		$this->insert('disorder',array('id' => 107,'fully_specified_name' => 'Primary Angle Closure (disorder)','term' => 'Primary Angle Closure'));
-		$this->insert('disorder',array('id' => 108,'fully_specified_name' => 'Primary acute angle-closure glaucoma (disorder)','term' => 'Primary acute angle-closure glaucoma'));
-		$this->insert('disorder',array('id' => 109,'fully_specified_name' => 'Secondary acute angle-closure glaucoma (disorder)','term' => 'Secondary acute angle-closure glaucoma'));
+		foreach (array(
+			array('id' => 104,'fully_specified_name' => 'Primary Ocular hypertension (disorder)','term' => 'Primary Ocular hypertension'),
+			array('id' => 105,'fully_specified_name' => 'Normal Tension Glaucoma (disorder)', 'term' => 'Normal Tension Glaucoma'),
+			array('id' => 106,'fully_specified_name' => 'Primary acute angle-closure glaucoma (disorder)','term' => 'Primary acute angle-closure glaucoma')
+			) as $disorder) {
+
+			if (!$this->dbConnection->createCommand()->select("id")->from("disorder")->where("id = :id",array(":id" => $disorder['id']))->queryScalar()) {
+				$this->insert('disorder', $disorder);
+			}
+		}
 
 		$fp = fopen(dirname(__FILE__)."/../data/oe-5154.csv","r");
 
@@ -32,44 +36,42 @@ class m141127_105011_g21_diagnoses_and_findings extends CDbMigration
 			}
 
 			if (@$data[1]) {
-				$cod = new CommonOphthalmicDisorder;
-				$cod->display_order = $parent_do++;
-				$cod->subspecialty_id = $glaucoma->id;
+				$cod = array();
+				$cod['display_order'] = $parent_do++;
+				$cod['subspecialty_id'] = $glaucoma_id;
 
 				if ($data[4]) {
 					if (!preg_match('/\[([0-9]+)\]/',$data[4],$m)) {
 						throw new Exception("Unable to read disorder: {$data[4]}");
 					}
 
-					if (!$disorder = Disorder::model()->findByPk($m[1])) {
+					if (!$disorder = $this->dbConnection->createCommand()->select("*")->from("disorder")->where("id = :id",array(":id" => $m[1]))->queryRow()) {
 						throw new Exception("Disorder not found: {$m[1]}");
 					}
 
-					$cod->disorder_id = $disorder->id;
+					$cod['disorder_id'] = $disorder['id'];
 
 					if ($data[6]) {
 						if (!preg_match('/\[([0-9]+)\]/',$data[6],$m)) {
 							throw new Exception("Unable to read disorder: {$data[6]}");
 						}
-						if (!$alt_disorder = Disorder::model()->findByPk($m[1])) {
+						if (!$alt_disorder = $this->dbConnection->createCommand()->select("*")->from("disorder")->where("id = :id",array(":id" => $m[1]))->queryRow()) {
 							throw new Exception("Disorder not found: {$m[1]}");
 						}
-						$cod->alternate_disorder_id = $alt_disorder->id;
-						$cod->alternate_disorder_label = $data[2];
+						$cod['alternate_disorder_id'] = $alt_disorder['id'];
+						$cod['alternate_disorder_label'] = $data[2];
 					}
 				} else {
-					if (!$finding = Finding::model()->find('name=?',array($data[5]))) {
+					if (!$finding = $this->dbConnection->createCommand()->select("*")->from("finding")->where("name = :name",array(":name" => $data[5]))->queryRow()) {
 						throw new Exception("Finding not found: {$data[5]}");
 					}
 
-					$cod->finding_id = $finding->id;
+					$cod['finding_id'] = $finding['id'];
 				}
 
-				if (!$cod->save()) {
-					throw new Exception("Unable to save cod: ".print_r($cod->errors,true));
-				}
+				$this->insert('common_ophthalmic_disorder',$cod);
 
-				$parent = $cod;
+				$parent = $this->dbConnection->createCommand()->select("*")->from("common_ophthalmic_disorder")->order('id desc')->limit(1)->queryRow();
 				$sec_do = 0;
 
 			} else if (@$data[2]) {
@@ -77,35 +79,33 @@ class m141127_105011_g21_diagnoses_and_findings extends CDbMigration
 					throw new Exception("Arrived at secondary item without parent.");
 				}
 
-				$sec = new SecondaryToCommonOphthalmicDisorder;
-				$sec->parent_id = $parent->id;
-				$sec->display_order = $sec_do++;
+				$sec = array();
+				$sec['parent_id'] = $parent['id'];
+				$sec['display_order'] = $sec_do++;
 
 				if ($data[7]) {
 					if (!preg_match('/\[([0-9]+)\]/',$data[7],$m)) {
 						throw new Exception("Unable to read disorder: {$data[7]}");
 					}
-					if (!$disorder = Disorder::model()->findByPk($m[1])) {
+					if (!$disorder = $this->dbConnection->createCommand()->select("*")->from("disorder")->where("id = :id",array(":id" => $m[1]))->queryRow()) {
 						throw new Exception("Disorder not found: {$m[1]}");
 					}
 
-					$sec->disorder_id = $disorder->id;
+					$sec['disorder_id'] = $disorder['id'];
 
 				} else {
-					if (!$finding = Finding::model()->find('name=?',array($data[8]))) {
+					if (!$finding = $this->dbConnection->createCommand()->select("*")->from("finding")->where("name = :name",array(":name" => $data[8]))->queryRow()) {
 						throw new Exception("Finding not found: {$data[8]}");
 					}
 
-					$sec->finding_id = $finding->id;
+					$sec['finding_id'] = $finding['id'];
 				}
 
 				if ($data[9]) {
-					$sec->letter_macro_text = $data[9];
+					$sec['letter_macro_text'] = $data[9];
 				}
 
-				if (!$sec->save()) {
-					throw new Exception("Unable to save secondary-to item: ".print_r($sec->errors,true));
-				}
+				$this->insert('secondaryto_common_oph_disorder',$sec);
 			}
 		}
 
@@ -114,13 +114,12 @@ class m141127_105011_g21_diagnoses_and_findings extends CDbMigration
 
 	public function down()
 	{
-		$glaucoma = Subspecialty::model()->find('name=?',array('Glaucoma'));
+		$glaucoma_id = $this->dbConnection->createCommand()->select("id")->from("subspecialty")->where("name=:name",array(":name" => "Glaucoma"))->queryScalar();
 
-		foreach (Yii::app()->db->createCommand()->select("*")->from("common_ophthalmic_disorder")->where("subspecialty_id=$glaucoma->id")->queryAll() as $cod) {
-			Yii::app()->db->createCommand("delete from secondaryto_common_oph_disorder where parent_id = {$cod['id']}")->query();
+		foreach ($this->dbConnection->createCommand()->select("*")->from("common_ophthalmic_disorder")->where("subspecialty_id=$glaucoma_id")->queryAll() as $cod) {
+			$this->dbConnection->createCommand("delete from secondaryto_common_oph_disorder where parent_id = {$cod['id']}")->query();
 		}
 
-		Yii::app()->db->createCommand("delete from common_ophthalmic_disorder where subspecialty_id=$glaucoma->id")->query();
-		Yii::app()->db->createCommand("delete from disorder where id in (104,105,106,107,108,109)")->query();
+		$this->dbConnection->createCommand("delete from common_ophthalmic_disorder where subspecialty_id=$glaucoma_id")->query();
 	}
 }
