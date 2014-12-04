4a059792d541aceb206ec963c4700cfb3d875519
Mike Smith <mike.smith@openeyes.org.uk>
2014-12-04 21:14:30 +0000

    + data corrections

--- migrations/m141204_195841_missing_data_migrations.php
+++ migrations/m141204_195841_missing_data_migrations.php
@@ -9,8 +9,8 @@ class m141204_195841_missing_data_migrations extends CDbMigration
 		//OE-4616
 		$this->update('socialhistory_smoking_status', array('name' => 'Unknown'), 'name = "Tobacco smoking consumption unknown"');
 		//OES-490
-		$this->update('socialhistory_driving_status', array('name' => 'Car/Motorcycle'), 'name = "Motor Vehicle"');
-		$this->update('socialhistory_driving_status', array('name' => 'HGV/Bus Drive'), 'name = "HGV"');
+		$this->update('socialhistory_driving_status', array('name' => 'Car/Motorcycle'), 'name = "Motor vehicle"');
+		$this->update('socialhistory_driving_status', array('name' => 'HGV/Bus Driver'), 'name = "HGV"');
 		//OES-491
 		$this->update('ophciexamination_instrument', array('name' => 'ORA IOPcc'), 'name = "ORA"');
 		//OES-495
@@ -23,12 +23,12 @@ class m141204_195841_missing_data_migrations extends CDbMigration
 	{
 		$this->update('socialhistory_occupation', array('name' => 'Sickness'), 'name = "Disability Benefits"');
 		$this->update('socialhistory_smoking_status', array('name' => 'Tobacco smoking consumption unknown'), 'name = "Unknown"');
-		$this->update('socialhistory_driving_status', array('name' => 'Motor Vehicle'), 'name = "Car/Motorcycle"');
+		$this->update('socialhistory_driving_status', array('name' => 'Motor vehicle'), 'name = "Car/Motorcycle"');
 		$this->update('socialhistory_driving_status', array('name' => 'HGV'), 'name = "HGV/Bus Drive"');
 		$this->update('ophciexamination_instrument', array('name' => 'ORA'), 'name = "ORA IOPcc"');
 		$this->update('ophciexamination_overallperiod', array('active' => true), 'name = "Discharge"');
-		$this->delete('ophciexamination_overallperiod', 'name = ?', 'Not required');
-		$this->delete('ophciexamination_overallperiod', 'name = ?', '18 months');
+		$this->delete('ophciexamination_overallperiod', 'name = ?', array('Not required'));
+		$this->delete('ophciexamination_overallperiod', 'name = ?', array('18 months'));
 	}
 
 	/*
