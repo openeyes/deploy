00c08d0c98670940c3541fe91bc55d9d33a44490
Mark Wadham <m@rkw.me>
2014-12-09 11:02:35 +0000

    bugfix

--- migrations/m141205_123921_ticket_assign_outcome_option.php
+++ migrations/m141205_123921_ticket_assign_outcome_option.php
@@ -4,11 +4,13 @@ class m141205_123921_ticket_assign_outcome_option extends CDbMigration
 {
 	public function up()
 	{
-		$this->insert('patientticketing_ticketassignoutcomeoption',array('id'=>1,'name'=>'Discharge','display_order'=>0,'episode_status_id'=>5,'followup'=>1));
+		$this->insert('patientticketing_ticketassignoutcomeoption',array('id'=>1,'name'=>'Discharge','display_order'=>0,'episode_status_id'=>6,'followup'=>0));
+		$this->insert('patientticketing_ticketassignoutcomeoption',array('id'=>2,'name'=>'Follow up','display_order'=>1,'episode_status_id'=>5,'followup'=>1));
 	}
 
 	public function down()
 	{
 		$this->delete('patientticketing_ticketassignoutcomeoption',"id=1");
+		$this->delete('patientticketing_ticketassignoutcomeoption',"id=2");
 	}
 }
