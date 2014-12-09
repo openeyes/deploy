3fb84915f3df7e8054094aa673da734b04af6295
Mike Smith <mike.smith@openeyes.org.uk>
2014-12-09 19:40:20 +0000

    + Defensive migrations for missing data after accidental dupes in external deployment

--- migrations/m141204_195841_missing_data_migrations.php
+++ migrations/m141204_195841_missing_data_migrations.php
@@ -5,18 +5,37 @@ class m141204_195841_missing_data_migrations extends CDbMigration
 	public function up()
 	{
 		//OES-489
-		$this->update('socialhistory_occupation', array('name' => 'Disability Benefits'), 'name = "Sickness"');
+		if ($this->dbConnection->createCommand()->select("*")->from("socialhistory_occupation")->where("name = :n",array(":n" => "Sickness"))->queryRow()) {
+			$this->update('socialhistory_occupation', array('name' => 'Disability Benefits'), 'name = "Sickness"');
+		}
+
 		//OE-4616
-		$this->update('socialhistory_smoking_status', array('name' => 'Unknown'), 'name = "Tobacco smoking consumption unknown"');
+		if ($this->dbConnection->createCommand()->select("*")->from("socialhistory_smoking_status")->where("name = :n",array(":n" => "Tobacco smoking consumption unknown"))->queryRow()) {
+			$this->update('socialhistory_smoking_status', array('name' => 'Unknown'), 'name = "Tobacco smoking consumption unknown"');
+		}
+
 		//OES-490
-		$this->update('socialhistory_driving_status', array('name' => 'Car/Motorcycle'), 'name = "Motor vehicle"');
-		$this->update('socialhistory_driving_status', array('name' => 'HGV/Bus Driver'), 'name = "HGV"');
+		if ($this->dbConnection->createCommand()->select("*")->from("socialhistory_driving_status")->where("name = :n",array(":n" => "Motor vehicle"))->queryRow()) {
+			$this->update('socialhistory_driving_status', array('name' => 'Car/Motorcycle'), 'name = "Motor vehicle"');
+		}
+
+		if ($this->dbConnection->createCommand()->select("*")->from("socialhistory_driving_status")->where("name = :n",array(":n" => "HGV"))->queryRow()) {
+			$this->update('socialhistory_driving_status', array('name' => 'HGV/Bus Driver'), 'name = "HGV"');
+		}
+
 		//OES-491
-		$this->update('ophciexamination_instrument', array('name' => 'ORA IOPcc'), 'name = "ORA"');
+		if ($this->dbConnection->createCommand()->select("*")->from("ophciexamination_instrument")->where("name = :n",array(":n" => "ORA"))->queryRow()) {
+			$this->update('ophciexamination_instrument', array('name' => 'ORA IOPcc'), 'name = "ORA"');
+		}
+
 		//OES-495
 		$this->update('ophciexamination_overallperiod', array('active' => false), 'name = "Discharge"');
-		$this->insert('ophciexamination_overallperiod', array('name' => 'Not required', 'display_order' => 0));
-		$this->insert('ophciexamination_overallperiod', array('name' => '18 months', 'display_order' => 8));
+		if (!$this->dbConnection->createCommand()->select("*")->from("ophciexamination_overallperiod")->where("name = :n",array(":n" => "Not required"))->queryRow()) {
+			$this->insert('ophciexamination_overallperiod', array('name' => 'Not required', 'display_order' => 0));
+		}
+		if (!$this->dbConnection->createCommand()->select("*")->from("ophciexamination_overallperiod")->where("name = :n",array(":n" => "18 months"))->queryRow()) {
+			$this->insert('ophciexamination_overallperiod', array('name' => '18 months', 'display_order' => 8));
+		}
 	}
 
 	public function down()
