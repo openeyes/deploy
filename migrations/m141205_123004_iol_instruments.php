ef267ff23d2d06e5e59559efda5c9163a4c9f852
Mike Smith <mike.smith@openeyes.org.uk>
2015-01-06 15:06:12 +0000

    + ORA IOPg has been added manually live, so no longer want to insert it during deployment

--- migrations/m141205_123004_iol_instruments.php
+++ migrations/m141205_123004_iol_instruments.php
@@ -4,6 +4,7 @@ class m141205_123004_iol_instruments extends CDbMigration
 {
 	public $map = array(
 		'ORAcc' => 'ORA IOPcc',
+		'ORAg' => 'ORA IOPg',
 		'Palp' => 'Palpation',
 		'DCT' => 'Dynamic Contour Tonometry',
 		'GAT' => 'Goldmann',
@@ -12,8 +13,6 @@ class m141205_123004_iol_instruments extends CDbMigration
 
 	public function up()
 	{
-		$this->insert('ophciexamination_instrument',array('name'=>'ORA IOPg','short_name'=>'ORAg','display_order'=>65,'active'=>1));
-
 		foreach ($this->map as $short_name => $name) {
 			$this->update('ophciexamination_instrument',array('short_name' => $short_name),"name = '$name'");
 		}
@@ -21,8 +20,6 @@ class m141205_123004_iol_instruments extends CDbMigration
 
 	public function down()
 	{
-		$this->delete('ophciexamination_instrument',"name = 'ORA IOPg'");
-
 		foreach ($this->map as $short_name => $name) {
 			$this->update('ophciexamination_instrument',array('short_name' => null),"name = '$name'");
 		}
