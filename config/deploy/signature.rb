f0845ad7fcc16541b6ba03ef1c5e0721d9d10df0
petergallagher <peter.s.gallagher@gmail.com>
2016-09-21 16:10:05 +0100

    Link file for config in signature.

--- config/deploy/signature.rb
+++ config/deploy/signature.rb
@@ -1,5 +1,6 @@
 set :application, 'Signature'
 set :repo_url, 'https://github.com/openeyes/signature-capture.git'
+set :linked_files, fetch(:linked_files, []).push('src/config/config.js')
 
 Rake::Task['deploy:updated'].prerequisites.delete('npm:install')
 Rake::Task['deploy:updated'].prerequisites.delete('bower:install')
